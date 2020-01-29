18745698ef8f3854c4aa10a4c44bb6d260c24c64
João Pedro Moraes <joaopedromoraes@outlook.com>
2020-01-29 20:19:07 -0300

    FEAT: Cria os componentes da aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListagemComponent } from './components/listagem/listagem.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { ListaComponent } from './components/listagem/lista/lista.component';
+import { PaginationComponent } from './components/listagem/pagination/pagination.component';
+import { BuscaComponent } from './components/listagem/busca/busca.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListagemComponent,
+    CadastroComponent,
+    ListaComponent,
+    PaginationComponent,
+    BuscaComponent
   ],
   imports: [
     BrowserModule,
