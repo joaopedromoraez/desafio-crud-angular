04b3b529496a0a646d1c1e69cbd9f4d07785fcf9
João Pedro Moraes <joaopedromoraes@outlook.com>
2020-01-30 21:34:02 -0300

    FEAT: Finaliza a função de busca na pagina de listagem de usuarios

--- src/app/components/listagem/listagem.component.ts
+++ src/app/components/listagem/listagem.component.ts
@@ -8,13 +8,14 @@ import { ApiService } from 'src/app/services/api.service';
 })
 export class ListagemComponent implements OnInit {
 
-  usuarios = [];
+  usuariosPaginado = [];
   usuariosTotal = [];
+  usuariosBusca = [];
   paginasTotais: number;
   paginaAtual: number = 1;
   itensPagina: number = 10;
   pagination = [];
-  busca: string = ""
+  busca: string = "";
 
 
   constructor(private apiService: ApiService) { }
@@ -23,40 +24,56 @@ export class ListagemComponent implements OnInit {
     this.apiService.listar()
       .then(response => {
         this.usuariosTotal = response;
-        this.usuarios = response.slice(0, this.itensPagina);
-        this.paginasTotais = Math.ceil(response.length / this.itensPagina);3
-        for (let x = 0; x < this.paginasTotais; x++){
-          this.pagination.push(x+1);
+        this.usuariosBusca = response;
+        this.usuariosPaginado = response.slice(0, this.itensPagina);
+        this.paginasTotais = Math.ceil(response.length / this.itensPagina);
+        for (let x = 0; x < this.paginasTotais; x++) {
+          this.pagination.push(x + 1);
         }
       })
   }
 
   proximaPagina() {
     if (this.paginaAtual !== this.paginasTotais) {
-      this.usuarios = [];
-      this.usuarios = this.usuariosTotal.slice(this.paginaAtual * this.itensPagina, (this.paginaAtual * this.itensPagina) + this.itensPagina);
+      this.usuariosPaginado = this.usuariosBusca.slice(this.paginaAtual * this.itensPagina, (this.paginaAtual * this.itensPagina) + this.itensPagina);
       this.paginaAtual++;
     }
   }
 
   anteriorPagina() {
     if (this.paginaAtual !== 1) {
-      this.usuarios = [];
       this.paginaAtual--;
-      this.usuarios = this.usuariosTotal.slice((this.paginaAtual - 1) * this.itensPagina, ((this.paginaAtual - 1) * this.itensPagina) + this.itensPagina);
+      this.usuariosPaginado = this.usuariosBusca.slice((this.paginaAtual - 1) * this.itensPagina, ((this.paginaAtual - 1) * this.itensPagina) + this.itensPagina);
     }
   }
 
-  paginacao(pagina: any){
-    this.usuarios = [];
+  paginacao(pagina: number) {
     this.paginaAtual = pagina;
-    this.usuarios = this.usuariosTotal.slice((this.paginaAtual - 1) * this.itensPagina, ((this.paginaAtual - 1) * this.itensPagina) + this.itensPagina);
+    this.usuariosPaginado = this.usuariosBusca.slice((this.paginaAtual - 1) * this.itensPagina, ((this.paginaAtual - 1) * this.itensPagina) + this.itensPagina);
   }
 
   buscaUsuario() {
-    this.usuarios = this.usuariosTotal.filter((pokemon)=>{
-      return pokemon.name.indexOf(this.busca) !== -1;
-    });
-    // console.table(this.listaDepokemons);
+    if (this.busca !== null) {
+      this.usuariosBusca = this.usuariosTotal.filter((usuario) => {
+        return usuario.name.toLowerCase().indexOf(this.busca.toLocaleLowerCase()) !== -1;
+      });
+      this.usuariosPaginado = this.usuariosBusca.slice(0, this.itensPagina);
+      this.paginasTotais = Math.ceil(this.usuariosBusca.length / this.itensPagina);
+      this.pagination = []
+      for (let x = 0; x < this.paginasTotais; x++) {
+        this.pagination.push(x + 1);
+      }
+
+    } else {
+
+      this.usuariosPaginado = this.usuariosTotal.slice(0, this.itensPagina);
+      this.paginasTotais = Math.ceil(this.usuariosTotal.length / this.itensPagina);
+      this.pagination = [];
+      for (let x = 0; x < this.paginasTotais; x++) {
+        this.pagination.push(x + 1);
+      }
+      
+    }
   }
+
 }
